5183bf1a4351551410f741f0ca86da943cab028e
Sascha Jongebloed <sasjonge@uni-bremen.de>
2017-01-26 16:30:02 +0100

    Added possibility for own foodlists

--- heyfood/MailClient/SendAMail.py
+++ heyfood/MailClient/SendAMail.py
@@ -6,19 +6,19 @@ from getpass         import getpass
 from smtplib         import SMTP_SSL
 
 
-def send_mail(me, pwd, goals, subject, text):
+def send_mail(me, pwd, goal, subject, text):
 
     # create message
     msg = MIMEText(text, 'plain', 'utf-8')
     msg['Subject'] = Header(subject, 'utf-8')
     msg['From'] = me
-    msg['To'] = ", ".join(goals)
+    msg['To'] = ", ".join([goal])
 
     # send it via gmail
     s = SMTP_SSL('smtp.gmail.com', 465, timeout=10)
     s.set_debuglevel(1)
     try:
         s.login(me, pwd)
-        s.sendmail(msg['From'], goals, msg.as_string())
+        s.sendmail(msg['From'], [goal], msg.as_string())
     finally:
         s.quit()
